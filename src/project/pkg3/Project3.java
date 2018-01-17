40cd0e308d398a8b5fa34d6d1b992248cbcb7261
Muhammad Farhan <Muhammad Farhan@LAPTOP-6I5DS4U1>
2018-01-17 23:18:30 +0800

    version 2

--- src/project/pkg3/Project3.java
+++ src/project/pkg3/Project3.java
@@ -16,28 +16,28 @@ public class project3 {
         
         int kod;
         
-        System.out.println("Ini adalah sistem yang menunjukkan bilangan buku mengikut kod buku.");
-        System.out.print("Masukkan kod buku :");
+        System.out.println("Ini adalah sistem yang menunjukkan bilangan buku mengikut kod sumber.");
+        System.out.print("Masukkan kod sumber :");
         kod=input.nextInt();
         
         if (kod==100) {
-            System.out.println("Bilangan buku bagi kod 100 adalah 12");
+            System.out.println("Bilangan sumber bagi kod 100 adalah 12");
         }
         else if (kod==101) {
-            System.out.println("Bilangan buku bagi kod 101 adalah 5");
+            System.out.println("Bilangan sumber bagi kod 101 adalah 5");
         }
         else if (kod==111) {
-            System.out.println("Bilangan buku bagi kod 111 adalah 2");
+            System.out.println("Bilangan sumber bagi kod 111 adalah 2");
         }
         else if (kod==113) {
-            System.out.println("Bilangan buku bagi kod 113 adalah 16");
+            System.out.println("Bilangan sumber bagi kod 113 adalah 16");
         }
         else if (kod==145) {
-            System.out.println("Bilangan buku bagi kod 145 adalah 25");
+            System.out.println("Bilangan sumber bagi kod 145 adalah 25");
         }
         else {
             System.out.println("Tiada data dalam sistem.");
-            System.out.print("Masukkan kod buku yang betul :");
+            System.out.print("Masukkan kod sumber yang betul :");
             kod=input.nextInt();
         }
         System.out.println(+kod);
